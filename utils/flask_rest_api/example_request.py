b4beb238cb1e48daccca2032321b6df485aad676
77svene <201388040+77svene@users.noreply.github.com>
2026-03-18 17:01:47 -0400

    Add utils/flask_rest_api/example_request.py

---
+++ utils/flask_rest_api/example_request.py
@@ -0,0 +1,17 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+"""Perform test request."""
+
+import pprint
+
+import requests
+
+DETECTION_URL = "http://localhost:5000/v1/object-detection/nexuss"
+IMAGE = "zidane.jpg"
+
+# Read image
+with open(IMAGE, "rb") as f:
+    image_data = f.read()
+
+response = requests.post(DETECTION_URL, files={"image": image_data}).json()
+
+pprint.pprint(response)
